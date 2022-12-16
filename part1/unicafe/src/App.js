2b263674868241c76088b1e55cd236495f877f89
james <zyh.me@outlook.com>
2022-12-16 17:24:04 +0800

    Part 1: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,32 +4,33 @@ const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}> {text} </button>
 );
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ data: { good, neutral, bad } }) => {
   const all = good.value + bad.value + neutral.value;
   const average = all === 0 ? 0 : (good.value - bad.value) / all;
   const positive = all === 0 ? 0 : (good.value * 100) / all + "%";
-  
+
   if (all === 0) {
-    return <div>No feedback given</div>
+    return <div>No feedback given</div>;
   }
 
   return (
-    <div>
-      <Display text={good.name} value={good.value} />
-      <Display text={neutral.name} value={neutral.value} />
-      <Display text={bad.name} value={bad.value} />
-
-      <Display text="all" value={all} />
-      <Display text="average" value={average} />
-      <Display text="positive" value={positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text={good.name} value={good.value} />
+        <StatisticLine text={neutral.name} value={neutral.value} />
+        <StatisticLine text={bad.name} value={bad.value} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   );
 };
 
-const Display = ({ text, value }) => (
-  <div>
-    {text} {value}
-  </div>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td> <td>{value}</td>
+  </tr>
 );
 
 const App = () => {
@@ -54,14 +55,14 @@ const App = () => {
   const increaseBad = () => setBad({ ...bad, value: bad.value + 1 });
 
   return (
-    <div>
+    <>
       <h1>give feedback</h1>
       <Button handleClick={increaseGood} text={good.name} />
       <Button handleClick={increaseNeutral} text={neutral.name} />
       <Button handleClick={increaseBad} text={bad.name} />
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
+      <Statistics data={{ good, neutral, bad }} />
+    </>
   );
 };
 
