39b8ecb72a20f9c35e4fc902dbb978ff0abd2ed5
james <zyh.me@outlook.com>
2022-12-21 00:57:25 +0800

    Part2: courseinfo step8

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,11 +1,12 @@
-const Course = ( {course} ) => {
-
-  return <div>
+const Course = ({ course }) => {
+  return (
+    <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-  </div>
-}
+    </div>
+  );
+};
 const Header = (props) => {
   return (
     <div>
@@ -27,21 +28,21 @@ const Content = ({ parts }) => {
 const Part = ({ part }) => {
   return (
     <div>
-      <p>  {part.name} {part.exercises} </p>
+      <p>
+        {" "}
+        {part.name} {part.exercises}{" "}
+      </p>
     </div>
   );
 };
 
-const Total = ({parts}) => {
-  let sum = 0;
-  for (let p of parts) {
-    sum += p.exercises;
-  }
-
-  return <div><b>total of {sum} exercises</b></div>;
-}
-
-
+const Total = ({ parts }) => {
+  return (
+    <div>
+      <b>total of {parts.reduce((p, c) => p + c.exercises, 0)} exercises</b>
+    </div>
+  );
+};
 
 const App = () => {
   const course = {
@@ -51,6 +52,7 @@ const App = () => {
       { name: "Fundamentals of React", exercises: 10, id: 1 },
       { name: "Using props to pass data", exercises: 7, id: 2 },
       { name: "State of a component", exercises: 14, id: 3 },
+      { name: "Redux", exercises: 11, id: 4 },
     ],
   };
 
